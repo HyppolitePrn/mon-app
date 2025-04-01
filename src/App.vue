65c0b93bbfbc9fc6333105dc1997dcfb30ec2cd3
HyppolitePrn <pernothyppolite@gmail.com>
2025-04-01 11:27:47 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header class="bg-red-300">Title</header>
+
+  <RouterView />
+
+  <footer>Copyright</footer>
+</template>
+
+<style scoped></style>
